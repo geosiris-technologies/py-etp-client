ad3071dafacfcf072846a7457fa96ab7e733a9bc
Valentin Gauthier <valentin.gauthier@geosiris.com>
2025-10-07 16:54:00 +0200

    --

--- example/py_etp_client_example/ui.py
+++ example/py_etp_client_example/ui.py
@@ -593,4 +593,15 @@ def looper():
 
 if __name__ == "__main__":
 
+    from py_etp_client.serverprotocols import (
+        CoreProtocolPrinter,
+        DiscoveryProtocolPrinter,
+        DataspaceHandlerPrinter,
+        StoreProtocolPrinter,
+        DataArrayHandlerPrinter,
+        SupportedTypesProtocolPrinter,
+        TransactionHandlerPrinter,
+        enable_logs,
+    )
+
     looper()
